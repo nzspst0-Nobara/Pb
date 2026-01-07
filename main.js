ed335335ba36f89e8e8865d07a8b39b02d5a9d10
Enzo <nzspst0@gmail.com>
2026-01-07 17:31:42 -0400

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-// main.js - add custom interactivity later
-document.querySelectorAll('.buy-btn').forEach(btn => {
-    btn.addEventListener('click', () => {
-        alert('To purchase this boost, please contact us on Discord or Telegram!\n\nDiscord: nzspst0\nTelegram: @ernismarSF');
-    });
-});
