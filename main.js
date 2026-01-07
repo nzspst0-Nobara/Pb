08e9716c5ea193f0912a349ff652b621618451af
Enzo <nzspst0@gmail.com>
2026-01-07 01:17:04 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// main.js - add custom interactivity later
+document.querySelectorAll('.buy-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        alert('To purchase this boost, please contact us on Discord or Telegram!\n\nDiscord: nzspst0\nTelegram: @ernismarSF');
+    });
+});
